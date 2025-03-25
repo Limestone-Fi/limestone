f641da3b63d3f42189b499b2e3b7afee7fae3d6b
chainvisions <chainvisions@outlook.com>
2025-03-25 17:58:36 -0600

    Milady

---
+++ cancun/src/Milady.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Unlicense
+// There is no license. I love you.
+pragma solidity 0.8.28;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+import {ERC1967Factory} from "solady/src/utils/ERC1967Factory.sol";
+
+contract Milady {}
